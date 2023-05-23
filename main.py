4cb55e7daaac121e5bd23050636e9ddc96b35d51
Paul Leber <luaprebel@gmail.com>
2023-05-23 08:52:21 -0700

    basic app

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def hello():
+    return "Hello world"
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
