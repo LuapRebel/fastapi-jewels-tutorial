93c31e8dca6ea4e23c2508c83672114abf1573b8
Paul Leber <luaprebel@gmail.com>
2023-05-23 17:13:47 -0700

    update get functionality and session location

--- db/db.py
+++ db/db.py
@@ -1,8 +1,9 @@
 from pathlib import Path
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine, Session, SQLModel
 
 db_path = f"sqlite:///{Path(__file__).parent.parent}/database.db"
 engine = create_engine(db_path, echo=True)
+session = Session(bind=engine)
 
 
 def create_db_and_tables():
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI
 from pathlib import Path
 from db.db import create_db_and_tables
 from repos.gem_repository import select_all_gems, select_gem
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine, SQLModel, select
 import uvicorn
 
+from db.db import session
 from models.gem_models import *
 
 
@@ -13,14 +14,16 @@ app = FastAPI()
 
 @app.get("/gems")
 def gems():
-    all_gems = select_all_gems()
-    return all_gems
+    with session as s:
+        statement = select(Gem)
+        results = session.exec(statement)
+        return results.all()
 
 
-@app.get("/gems/{id}")
+@app.get("/gems/{id}", response_model=Gem)
 def gem(id: int):
-    gem = select_gem(id)
-    return {"gem": gem}
+    gem = session.get(Gem, id)
+    return gem
 
 
 if __name__ == "__main__":
